da8bb36c2b8cab5e8fe8b35dfa99ea2c5d9fd035
BethAnn Hanlon <herbivore137@gmail.com>
2019-06-03 22:43:34 +0000

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,8 +1,8 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
 
   def change
-    create_table :students do |s|
-      s.string :name
+    create_table :students do |t|
+      t.string :name
     end
   end
 
